65a4aef18f1ed7456b682b39ac45d4e0bc6fae23
Maciej Weber <maciejwebber@gmail.com>
2022-01-21 15:16:42 +0100

    uncaught exceptions

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPION!');
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
+
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
