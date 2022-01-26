7c0ec518f5ceced6c652bd154012ac6de3eb2334
Maciej Weber <maciejwebber@gmail.com>
2022-01-26 22:22:06 +0100

    signup and login

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please tell us you name!'],
     unique: true,
+    required: [true, 'Please tell us you name!'],
     maxlength: [20, 'A tour name must have less or equal then 20 characters'],
     minlength: [5, 'A tour name must have more or equal then 5 characters']
   },
   email: {
     type: String,
-    required: [true, 'Please provide your email'],
     unique: true,
+    required: [true, 'Please provide your email'],
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email']
   },
@@ -22,13 +23,33 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minlength: 8
+    minlength: 8,
+    select: false
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password']
+    required: [true, 'Please confirm your password'],
+    validate: function(el) {
+      return el === this.password;
+    },
+    message: 'Passwords are not the same!'
   }
 });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
