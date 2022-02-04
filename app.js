c5fbe641efaaa4dd08a3135ef02d5324a2bba75a
Maciej Weber <maciejwebber@gmail.com>
2022-02-04 13:37:45 +0100

    review model

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError = require('./utlis/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -52,6 +53,7 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on the server!`, 404));
