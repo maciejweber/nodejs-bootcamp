bac7aeb6b97e5e87740dcede9656ff76d6bf498b
Maciej Weber <maciejwebber@gmail.com>
2022-02-04 23:11:14 +0100

    handler factory

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const AppError = require('../utlis/appError');
 const catchAsync = require('./../utlis/catchAsync');
+const factory = require('./handlerFactory');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -73,9 +74,4 @@ exports.updateUser = (req, res) => {
     message: 'This route is not yet defined!'
   });
 };
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
+exports.deleteUser = factory.deleteOne(User);
