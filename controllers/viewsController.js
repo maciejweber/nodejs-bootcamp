7bd3c3072931d727c7b7c0a25bfaf701e31f351e
Maciej Weber <maciejwebber@gmail.com>
2022-02-11 15:46:57 +0100

    building account page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -32,3 +32,9 @@ exports.getLoginForm = catchAsync(async (req, res, next) => {
     title: 'Log into you account'
   });
 });
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account'
+  });
+};
